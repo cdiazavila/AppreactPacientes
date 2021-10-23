e3bd05880ce66723a7336e54010977aa10fb88e2
Carlos Diaz <cdiazavila@correo.unicordoba.edu.co>
2021-10-22 19:38:01 -0500

    CRUD terminado con MERN

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,8 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Link, withRouter } from 'react-router-dom';
 import clienteAxios from '../config/axios';
 import Swal from 'sweetalert2';
 const Cita = (props) => {
+
+
+    // creamos un estatus para editar la cita 
+    const [citaEditar, guardarCitaEditar] = useState([
+        { nombre: '' },
+        { propietario: '' },
+        { fecha: '' },
+        { hora: '' },
+        { telefono: '' },
+        { sintomas: '' }
+    ]);
 
 
     if (!props.cita) {
@@ -13,6 +24,7 @@ const Cita = (props) => {
 
     // extraer por props
     const { cita: { _id, nombre, propietario, fecha, hora, telefono, sintomas } } = props;
+
 
     // motodo para eliminar una cita 
     const eliminarCita = id => {
@@ -34,8 +46,8 @@ const Cita = (props) => {
                     'Deleted!',
                     'Your file has been deleted.',
                     'success'
-                ) 
-                
+                )
+
                 // Eliminado de la base de datos
                 clienteAxios.delete(`/pacientes/${id}`)
                     .then(respuesta => {
@@ -48,10 +60,70 @@ const Cita = (props) => {
                     })
             }
         })
-      
+
 
 
     }
+    //catturar datos
+    const capturarDatos = e => {
+        document.querySelector('#nombre').value = nombre
+        document.querySelector('#propietario').value = propietario
+        document.querySelector('#telefono').value = telefono
+        document.querySelector('#fecha').value = fecha
+        document.querySelector('#hora').value = hora
+        document.querySelector('#sintomas').value = sintomas
+
+    }
+
+
+    // motodo para actualizar una cita 
+    const Actualizar = (id) => {
+        citaEditar[0].nombre = document.querySelector('#nombre').value;
+        citaEditar[0].propietario = document.querySelector('#propietario').value;
+        citaEditar[0].telefono = document.querySelector('#telefono').value;
+        citaEditar[0].fecha = document.querySelector('#fecha').value;
+        citaEditar[0].hora = document.querySelector('#hora').value;
+        citaEditar[0].sintomas = document.querySelector('#sintomas').value;
+      
+
+         Swal.fire({
+             title: '¿Estas seguro?',
+             text: "De editar esta cita",
+             icon: 'warning',
+             showCancelButton: true,
+         confirmButtonColor: '#3085d6',
+             cancelButtonColor: '#d33',
+             confirmButtonText: 'Si, Editar!',
+         cancelButtonText: 'Cancelar'
+         }).then((result) => {
+             if (result.value) {
+
+                // Alerta de eliminado
+                 Swal.fire(
+                     'Editado!',
+                     'Se edito correctamente.',
+                     'success'
+                )
+
+        // Eliminado de la base de datos
+        clienteAxios.put(`/pacientes/${id}`, citaEditar[0])
+            .then(respuesta => {
+                props.guardarConsultar(true);
+                props.history.push('/');
+
+            })
+            .catch(error => {
+                console.log(error)
+            })
+             }
+         })
+
+
+
+
+    }
+
+
     return (
         <Fragment>
             <h1 className="my-5">Nombre cita: {nombre}</h1>
@@ -89,12 +161,114 @@ const Cita = (props) => {
                                         Eliminar &times;
                                     </button>}
                                 </div>
+                                <div className="d-flex">
+                                    <button
+                                        type="button"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal"
+                                        className="text-uppercase py-2 px-5 mt-5 font-weight-bold btn btn-primary col"
+                                        onClick={capturarDatos}
+                                    >
+                                        Actualizar
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Editar Paciente {nombre}</h5>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form
+                                className="bg-white p-5 bordered"
+                            >
+
+                                <div className="form-group">
+                                    <label htmlFor="nombre">Nombre Mascota</label>
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        id="nombre"
+                                        name="nombre"
+
+
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="propietario">Nombre Propietario</label>
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        id="propietario"
+                                        name="propietario"
+                                        placeholder="Nombre Propietario"
+
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="telefono">Teléfono</label>
+                                    <input
+                                        type="tel"
+                                        className="form-control form-control-lg"
+                                        id="telefono"
+                                        name="telefono"
+
+
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="fecha">Fecha Alta</label>
+                                    <input
+                                        type="date"
+                                        className="form-control form-control-lg"
+                                        id="fecha"
+                                        name="fecha"
+
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="hora">Hora Alta</label>
+                                    <input
+                                        type="time"
+                                        className="form-control form-control-lg"
+                                        id="hora"
+                                        name="hora"
+
+                                    />
+                                </div>
+
+                                <div className="form-group">
+                                    <label htmlFor="sintomas">Síntomas</label>
+                                    <textarea
+                                        className="form-control"
+                                        id="sintomas"
+                                        name="sintomas"
+                                        rows="6"
+
+                                    ></textarea>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={() => Actualizar(_id)}>Editar</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </Fragment>
     );
 }
